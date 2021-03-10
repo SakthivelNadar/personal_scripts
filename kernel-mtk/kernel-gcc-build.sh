ae4b4bb1b4003b11b00a522a0b7c59bcf973fe22
SparXFusion <s2234nadar@gmail.com>
2021-03-10 20:45:04 +0530

    kernel-mtk: kernel-gcc: Ask for kernel defconfing
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- kernel-mtk/kernel-gcc-build.sh
+++ kernel-mtk/kernel-gcc-build.sh
@@ -7,11 +7,17 @@
 green='\033[0;32m'
 echo -e "$green"
 
+# Defconfig
+if [[ -z ${KERNEL_DEFCONFIG} ]]; then
+    echo -n "Enter your kernel defconfig name: "
+    read -r NAME
+    CONFIG=${KERNEL_DEFCONFIG}
+fi
+
 # Main Environment
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG=oppo6771_18611_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
